a1163802371b666a77c283999b824fff36c5aa32
tvitcom <tvitcom@yandex.ru>
2017-02-03 11:08:23 +0200

    first yii2 app configs ready

---
+++ app/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=news',
+    'username' => 'news',
+    'password' => 'pass_to_news',
+    'charset' => 'utf8',
+];
---
+++ app/migrations/m130524_201442_init.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+class m130524_201442_init extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%person}}', [
+            'id' => $this->bigPrimaryKey(),
+            'username' => $this->string(64)->notNull()->unique(),
+            'auth_key' => $this->string(32)->notNull(),
+            'pass_hash' => $this->string(64)->notNull(),
+            'password_reset_token' => $this->string(128)->unique(),
+            'email' => $this->string(64)->notNull()->unique(),
+
+            'status' => $this->smallInteger(8)->notNull()->defaultValue(10),
+            'created_at' => $this->datetime(12)->notNull(),
+            'last_visit' => $this->datetime(12)->notNull(),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
