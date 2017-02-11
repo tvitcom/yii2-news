82c03b790f7c914d232a385f15222996127f0e17
tvitcom <tvitcom@yandex.ru>
2017-02-11 15:22:09 +0200

    fix grammatical error in README file

--- app/themes/basic/site/thanks.php
+++ app/themes/basic/site/thanks.php
@@ -8,5 +8,6 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?php echo Html::encode($this->title); ?></h1>
+    <?php if (\Yii::$app->session->hasFlash('success')) \Yii::$app->session->getFlash('success');?>
     <p>Thank you for you request. You can read their emails and follow our further instructions.</p>
 </div>
