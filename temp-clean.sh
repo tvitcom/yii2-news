afdcf253546aea2011a2ec57c2b462e75c387e0a
tvitcom <tvitcom@yandex.ru>
2017-02-14 03:45:55 +0200

    del rbac run files

--- temp-clean.sh
+++ temp-clean.sh
@@ -9,6 +9,10 @@ echo "www/assets/* files deleted.";
 rm -rf app/runtime/debug/mail/*.eml
 echo "runtime/debug/mail/*.eml files deleted.";
 
+rm -rf app/rbac/assignments.php
+rm -rf app/rbac/items.php
+echo "rbac/*run* files deleted.";
+
 > app/runtime/logs/app.log
 echo "app.log cleaned.";
 
